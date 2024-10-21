e351416b70822fb304ba8cb569178a96843fba78
galeev ruslan <ruslan.nafisovich@gmail.com>
2024-10-21 16:10:55 +0500

    initial project and add unit tests for intern

---
+++ SkillCheck/SkillCheck.Tests/Intern/ArrayTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillCheck.Tests.Intern
+{
+    public class ArrayTests
+    {
+        /*
+         * Тест на поиск максимального числа в массиве:
+         */
+
+        [Test]
+        public void Find_max_number_in_array_and_returns_max_value()
+        {
+            int result = ArrayHelper.Max(new int[] { 1, 5, 3, 9, 2 });
+            Assert.That(result, Is.EqualTo(9)); // Ожидается 9 как максимальное число
+        }
+
+        /*
+         * Тест на сортировку массива
+         */
+
+        [Test]
+        public void Sort_array_and_returns_sorted_array()
+        {
+            int[] result = ArrayHelper.Sort(new int[] { 3, 1, 4, 2 });
+            Assert.That(result, Is.EqualTo(new int[] { 1, 2, 3, 4 })); // Ожидается отсортированный массив
+        }
+    }
+}
---
+++ SkillCheck/SkillCheck.Tests/Intern/StringTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillCheck.Tests.Intern
+{
+    public class StringTests
+    {
+        /*
+         * Тест на обратную строку:
+         */
+
+        [Test]
+        public void Reverse_string_and_retuns_reversed_string()
+        {
+            string result = StringHelper.Reverse("hello");
+            Assert.That(result, Is.EqualTo("olleh")); // Ожидается "olleh" для строки "hello"
+        }
+
+        /*
+         * Тест на проверку палиндрома:
+         */
+
+        [Test]
+        public void Check_if_string_is_palindrome_and_returns_true()
+        {
+            bool result = StringHelper.IsPalindrome("madam");
+            Assert.IsTrue(result); // "madam" является палиндромом
+        }
+
+        [Test]
+        public void Check_if_string_is_not_palindrome_and_returns_false()
+        {
+            bool result = StringHelper.IsPalindrome("hello");
+            Assert.IsFalse(result); // "hello" не является палиндромом
+        }
+
+        /*
+         * Тест на конкатенацию строк:
+         */
+
+        [Test]
+        public void Concatenate_two_strings_and_returns_concatenated_string()
+        {
+            string result = StringHelper.Concatenate("Hello", "World");
+            Assert.That(result, Is.EqualTo("HelloWorld")); // Ожидается "HelloWorld" без пробела
+        }
+    }
+}

a7a7b7fa6f8cdbdbcda3c1efb22554838401760a
Nikita Doynikov <ndoynikov@yandex.ru>
2024-10-28 14:28:03 +0500

    создать статический класс PersonHelper и метод IsAdult пройти тест AgeTest

---
+++ SkillCheck/SkillCheck/Models/PersonHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace SkillCheck.Models
+{
+    public static class PersonHelper
+    {
+        // Метод возвращает true, если возраст 18 или больше, иначе false
+        public static bool IsAdult(int age)
+        {
+            return age >= 18;
+        }
+    }
+}

3eac6f1d0c066ca78639c9c3bc29cc96796c6d13
infinitelifeline <infinitelifeline37@gamail.com>
2024-10-29 01:23:46 +0500

    Реализованы методы для прохождения тестов

--- SkillCheck/SkillCheck/Logic/Calculator.cs
+++ SkillCheck/SkillCheck/Logic/Calculator.cs
@@ -1,12 +1,21 @@
-﻿namespace SkillCheck.Logic
+﻿using System.Data;
+
+namespace SkillCheck.Logic
 {
     public class Calculator
     {
         public int Calculate(string equation)
         {
-            int result = 0;
-
-            return result;
+            if (string.IsNullOrWhiteSpace(equation))
+            {
+                throw new ArgumentNullException("Выражение не должно быть пустым");
+            }
+            else
+            {
+                var DT = new DataTable();
+                var result = DT.Compute(equation, string.Empty);
+                return Convert.ToInt32(result);
+            }
         }
     }
 }
---
+++ SkillCheck/SkillCheck/Models/PersonHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace SkillCheck.Models
+{
+    public static class PersonHelper
+    {
+        public static bool IsAdult(int age)
+        {
+            if (age >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}

62865428b4c0e7e99ce64ca8e51399170f215b8e
Nikita Doynikov <ndoynikov@yandex.ru>
2024-10-28 14:59:44 +0500

    изменить метод Calculate класса Calculator пройти тесты, пройдено 8/10 тестов

--- SkillCheck/SkillCheck.Tests/Intern/HardLevel (required)/CalculatorTests.cs
+++ SkillCheck/SkillCheck.Tests/Intern/HardLevel (required)/CalculatorTests.cs
@@ -1,4 +1,7 @@
-﻿namespace SkillCheck.Tests.Intern.HardLevel__required_
+﻿using SkillCheck.Logic;
+using Xunit;
+
+namespace SkillCheck.Tests.Intern.HardLevel__required_
 {
     public class CalculatorTests
     {
@@ -6,24 +9,25 @@
          * Здесь тест необходимо только раскомментировать
          */
 
-        //private readonly Calculator _calculator = new Calculator();
+        private readonly Calculator _calculator = new Calculator();
 
-        //[Theory]
-        //[InlineData("1+1", 2)]
-        //[InlineData("2-1", 1)]
-        //[InlineData("3*2", 6)]
-        //[InlineData("6/3", 2)]
-        //[InlineData("2+3*4", 14)]
-        //[InlineData("(2+3)*4", 20)]
-        //[InlineData("10/2+5", 10)]
-        //[InlineData("5-3*2+8/4", 1)]
-        //[InlineData("10*(2+3)-4/2", 58)]
-        //[InlineData("15-3*(2*5)/6+6", 4)]
-        //public void Calculate_equation_of_string_and_return_result_of_calculate(string equation, int expectedResult)
-        //{
-        //    var actualResult = _calculator.Calculate(equation);
+        [Theory]
+        [InlineData("1+1", 2)]
+        [InlineData("2-1", 1)]
+        [InlineData("3*2", 6)]
+        [InlineData("6/3", 2)]
+        [InlineData("2+3*4", 14)]
+        [InlineData("(2+3)*4", 20)]
+        [InlineData("10/2+5", 10)]
+        [InlineData("5-3*2+8/4", 1)]
+        [InlineData("10*(2+3)-4/2", 58)]
+        [InlineData("15-3*(2*5)/6+6", 4)]
 
-        //    Assert.Equal(expectedResult, actualResult);
-        //}
+        public void Calculate_equation_of_string_and_return_result_of_calculate(string equation, int expectedResult)
+        {
+            var actualResult = _calculator.Calculate(equation);
+
+            Assert.Equal(expectedResult, actualResult);
+        }
     }
 }
--- SkillCheck/SkillCheck/Logic/Calculator.cs
+++ SkillCheck/SkillCheck/Logic/Calculator.cs
@@ -1,12 +1,21 @@
-﻿namespace SkillCheck.Logic
+﻿using System.Data;
+
+namespace SkillCheck.Logic
 {
     public class Calculator
     {
+        // Метод для вычисления математического выражения в виде строки
         public int Calculate(string equation)
         {
-            int result = 0;
+            if (string.IsNullOrWhiteSpace(equation))
+            {
+                throw new ArgumentException("Выражение не должно быть пустым.", nameof(equation));
+            }
 
-            return result;
+            // Используем DataTable для вычисления строки
+            var dataTable = new DataTable();
+            var result = dataTable.Compute(equation, string.Empty);
+            return Convert.ToInt32(result); // Преобразуем результат в целое число
         }
     }
 }
